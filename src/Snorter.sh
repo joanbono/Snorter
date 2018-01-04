a67f0fcffa8eb2cc9c564ae60992ca4aff9425f2
Md Nazrul Islam Ridoy <ridoy.computers@gmail.com>
2018-01-04 23:13:44 +0600

    Snort Updated to 2.9.11.1
    Version Update

--- src/Snorter.sh
+++ src/Snorter.sh
@@ -3,8 +3,8 @@
 # Description: Install automatically Snort + Barnyard2 + PulledPork
 # Author: Joan Bono (@joan_bono)
 # Contributor: Md. Nazrul Islam (@rbshadow), Goffinet (@goffinet)
-# Version: 1.0.0
-# Last Modified: rbshadow @ 20171120
+# Version: 1.0.2
+# Last Modified: rbshadow @ 20180104
 
 RED='\033[0;31m'
 ORANGE='\033[0;205m'
